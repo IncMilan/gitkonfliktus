4ba71247de014d77a5411f8840efdacd353697ab
IncMilan <78852980+IncMilan@users.noreply.github.com>
2022-03-09 11:54:01 +0100

    3.név deklarálás

--- src/konfliktuskezeles/Konfliktuskezeles.java
+++ src/konfliktuskezeles/Konfliktuskezeles.java
@@ -4,9 +4,10 @@ package konfliktuskezeles;
 public class Konfliktuskezeles {
 
     public static void main(String[] args) {
+        int i = 7;
         String nev1;
         String nev2="Milán";
-        
+        int nev3;
     }
     
 }
